224c5af4a8827e8ea16b4748d4c9c5e92957e00e
i-norden <iansnorden@gmail.com>
2021-11-03 16:06:48 -0500

    redis stats

---
+++ stats.go
@@ -0,0 +1,77 @@
+package redisethdb
+
+type Stat string
+
+const (
+	// Pool stats
+	HITS         = "Hits"
+	MISSES       = "Misses"
+	TIMEOUTS     = "Timeouts"
+	TOTAL_CONNS  = "TotalConns"
+	IDLE_CONNS   = "IdleConns"
+	STABLE_CONNS = "StaleConns"
+
+	// Data stats
+	DB_SIZE = "DbSize"
+
+	// Info info
+	SERVER_INFO        = "Server"
+	CLIENTS_INFO       = "Clients"
+	MEMORY_INFO        = "Memory"
+	PERSISTENCE_INFO   = "Persistence"
+	STATS_INFO         = "Stats"
+	REPLICATION_INFO   = "Replication"
+	CPU_INFO           = "CPU"
+	COMMAND_STATS_INFO = "Command"
+	CLUSTER_INFO       = "Cluster"
+	MODULES_INFO       = "Modules"
+	KEYSPACE_INFO      = "Keyspace"
+	ERROR_STATS_INFO   = "Errors"
+	ALL_INFO           = "All"
+	DEFAULT_INFO       = "Default"
+	EVERYTHING         = "Everything"
+)
+
+func (s Stat) String() string {
+	return string(s)
+}
+
+var poolStats = []Stat{
+	HITS,
+	MISSES,
+	TIMEOUTS,
+	TOTAL_CONNS,
+	IDLE_CONNS,
+	STABLE_CONNS,
+}
+
+var dataStats = []Stat{
+	DB_SIZE,
+}
+
+var infoStats = []Stat{
+	SERVER_INFO,
+	CLIENTS_INFO,
+	MEMORY_INFO,
+	PERSISTENCE_INFO,
+	STATS_INFO,
+	REPLICATION_INFO,
+	CPU_INFO,
+	COMMAND_STATS_INFO,
+	CLUSTER_INFO,
+	MODULES_INFO,
+	KEYSPACE_INFO,
+	ERROR_STATS_INFO,
+	ALL_INFO,
+	DEFAULT_INFO,
+	EVERYTHING,
+}
+
+func inList(str Stat, list []Stat) bool {
+	for _, el := range list {
+		if str == el {
+			return true
+		}
+	}
+	return false
+}
